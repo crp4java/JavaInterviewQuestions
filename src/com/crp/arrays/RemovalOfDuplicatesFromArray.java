ec10501cf97ff4a9c0856e845461d172cf3d09cf
Rajendra <Rajendra@192.168.0.15>
2018-02-26 11:20:55 +0530

    Updated with Changes

--- src/com/crp/arrays/RemovalOfDuplicatesFromArray.java
+++ src/com/crp/arrays/RemovalOfDuplicatesFromArray.java
@@ -3,7 +3,23 @@ package com.crp.arrays;
 public class RemovalOfDuplicatesFromArray {
 
 	public static void main(String[] args) {
-		// 
+int[] array = { 1, 1, 2, 2, 3, 3, 5, 5, 8, 9, 9 };
+		
+
+		for (int i = 0; i<array.length; i++) {
+			boolean isDuplicate = false;
+			for (int j = 0; j<i; j++) {
+				if (array[i] == array[j]) {
+					isDuplicate = true;
+					break;
+				}
+			}
+			if (!isDuplicate) {
+				System.out.print(array[i] + " ");
+			}
+		
+	}
+ 
 
 	}
 
