def66d3a12f4c307b26fe67902185086bdc9d4d1
Rajendra <Rajendra@Rajendra-PC>
2018-02-27 19:36:00 +0530

    Added Programs

---
+++ src/com/crp/strings/PalindromeCheck.java
@@ -0,0 +1,30 @@
+package com.crp.strings;
+
+import java.util.Scanner;
+
+/**
+ * @author Rajendra
+ *
+ */
+public class PalindromeCheck {
+	@SuppressWarnings("resource")
+	public static void main(String args[]) {
+		String str, rev = "";
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a string to check :");
+		str = sc.nextLine();
+
+		int length = str.length();
+
+		for (int i = length - 1; i >= 0; i--)
+			rev = rev + str.charAt(i);
+
+		if (str.equals(rev))
+			System.out.println(str + " : is a palindrome");
+		else
+			System.out.println(str + " : is not a palindrome");
+
+	}
+
+}
